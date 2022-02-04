5d577c9ad6e4465a76a4a5afaf345ef4c85388c5
Toxic254 <cyrus_g@outlook.com>
2022-02-04 13:11:56 +0300

    Initial Commit. Created tictactoe game with a 2D List.

---
+++ lib/main.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tic-Tac-Toe AI',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: const MyHomePage(title: 'Play Against AI'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  /// A list of 9 numbers at the start
+  var tiles = List.filled(9, 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text(widget.title)),
+      ),
+      body: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 1,
+            child: GridView.count(
+              crossAxisCount: 3,
+              children: [
+                for (var i = 0; i < 9; i++)
+                  InkWell(
+                    splashColor: Colors.red,
+                    onTap: (() {
+                      setState(() {
+                        tiles[i] = 1;
+                        _runAi();
+                      });
+                    }),
+                    child: Center(
+                      child: Text(tiles[i] == 0
+                          ? ''
+                          : tiles[i] == 1
+                              ? 'X'
+                              : '0'),
+                    ),
+                  )
+              ],
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(isWinning(1, tiles)
+                  ? 'You Won!'
+                  : isWinning(2, tiles)
+                      ? 'You Lost'
+                      : 'Your Move'),
+              OutlinedButton(
+                onPressed: () {
+                  setState(() {
+                    tiles = List.filled(9, 0);
+                  });
+                },
+                child: const Text('Restart Game'),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  _gradientBackground() {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          stops: [
+            0.1,
+            0.4,
+            0.6,
+            0.9,
+          ],
+          colors: [
+            Colors.yellow,
+            Colors.red,
+            Colors.indigo,
+            Colors.teal,
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _runAi() async {
+    /// Delay the AI to give you the feel of playing with a thinking AI.
+    await Future.delayed(const Duration(milliseconds: 150));
+
+    int? winning;
+    int? blocking;
+    int? normal;
+
+    for (var i = 0; i < 9; i++) {
+      var val = tiles[i];
+
+      if (val > 0) {
+        continue;
+      }
+
+      var future = [...tiles]..[i] = 2;
+
+      if (isWinning(2, future)) {
+        winning = i;
+      }
+
+      future[i] = 1;
+
+      if (isWinning(1, future)) {
+        blocking = i;
+      }
+
+      normal = i;
+    }
+    var move = winning ?? blocking ?? normal;
+
+    if (move != null) {
+      setState(() {
+        tiles[move] = 2;
+      });
+    }
+  }
+
+  bool isWinning(int who, List<int> tiles) {
+    return (tiles[0] == who && tiles[1] == who && tiles[2] == who) ||
+        (tiles[3] == who && tiles[4] == who && tiles[5] == who) ||
+        (tiles[6] == who && tiles[7] == who && tiles[8] == who) ||
+        (tiles[0] == who && tiles[4] == who && tiles[8] == who) ||
+        (tiles[2] == who && tiles[4] == who && tiles[6] == who) ||
+        (tiles[0] == who && tiles[3] == who && tiles[6] == who) ||
+        (tiles[1] == who && tiles[4] == who && tiles[7] == who) ||
+        (tiles[2] == who && tiles[5] == who && tiles[8] == who);
+  }
+}
